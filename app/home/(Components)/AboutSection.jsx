eb00b1cecdc705917d4782f535592eceb0b5817f
amankothardeveloper <amankothari.developer@gmail.com>
2026-02-21 22:27:39 +0530

    Update

--- app/home/(Components)/AboutSection.jsx
+++ app/home/(Components)/AboutSection.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import images from "../../utils/imagePaths";
+import aboutUsContent from "../(Data)/About.json";
 
 const AboutSection = () => {
+  const { badge, content, features, cta, author } = aboutUsContent;
+
   return (
     <section className="about-section section-padding fix">
       <div className="container">
         <div className="about-wrapper">
           <div className="row">
+            {/* LEFT SIDE */}
             <div className="col-lg-6">
               <div className="about-image-items">
+                {/* Counter Badge */}
                 <div className="counter-shape float-bob-y">
                   <div className="icon">
-                    <img src={images.icon1} alt="icon-img" />
+                    <img src={images.icon1} alt="icon" />
                   </div>
                   <div className="content">
                     <h3>
-                      <span className="count">6,561</span>+
+                      <span className="count">{badge.number}</span>{" "}
+                      {badge.label}
                     </h3>
                   </div>
                 </div>
+
+                {/* Video Button */}
                 <div className="video-box">
                   <a
                     href="https://www.youtube.com/watch?v=Cn4G2lZ_g2I"
@@ -27,143 +35,95 @@ const AboutSection = () => {
                     <i className="fa-solid fa-play" />
                     <img
                       src={images.circleText}
-                      alt="text-img"
+                      alt="circle-text"
                       className="text-circle"
                     />
                   </a>
                 </div>
+
+                {/* Main Image */}
                 <div
                   className="about-image-1 bg-cover wow fadeInLeft"
                   data-wow-delay=".3s"
                   style={{
-                    backgroundImage: 'url("assets/img/about/01.jpg")',
-                    visibility: "hidden",
-                    animationDelay: "0.3s",
-                    animationName: "none",
+                    backgroundImage: `url(${images.about01})`,
                   }}
                 >
                   <div
                     className="about-image-2 wow fadeInUp"
                     data-wow-delay=".5s"
-                    style={{
-                      visibility: "hidden",
-                      animationDelay: "0.5s",
-                      animationName: "none",
-                    }}
                   >
-                    <img src={images.about02} alt="about-img" />
+                    <img src={images.about02} alt="about" />
                   </div>
                 </div>
               </div>
             </div>
+
+            {/* RIGHT SIDE */}
             <div className="col-lg-6 mt-4 mt-lg-0">
               <div className="about-content">
+                {/* Section Title */}
                 <div className="section-title">
-                  <span
-                    className="wow fadeInUp"
-                    style={{ visibility: "hidden", animationName: "none" }}
-                  >
-                    About Us
-                  </span>
-                  <h2
-                    className="wow fadeInUp"
-                    data-wow-delay=".3s"
-                    style={{
-                      visibility: "hidden",
-                      animationDelay: "0.3s",
-                      animationName: "none",
-                    }}
-                  >
-                    About Green Energy Solar <br /> &amp; energy
+                  <span className="wow fadeInUp">{content.tag}</span>
+
+                  <h2 className="wow fadeInUp" data-wow-delay=".3s">
+                    {content.title.line1}
+                    <br />
+                    {content.title.line2}
                   </h2>
                 </div>
-                <p
-                  className="mt-3 mt-md-0 wow fadeInUp"
-                  data-wow-delay=".5s"
-                  style={{
-                    visibility: "hidden",
-                    animationDelay: "0.5s",
-                    animationName: "none",
-                  }}
-                >
-                  Solar companies can offer solar panels, inverters, batteries,
-                  and other related services to help individuals businesses
-                  transition to energy sources businesses transition to energy
-                  sources and sustainable energy alternative.
+
+                {/* Description */}
+                <p className="mt-3 mt-md-0 wow fadeInUp" data-wow-delay=".5s">
+                  {content.description}
                 </p>
+
+                {/* Features */}
                 <div className="about-icon-items">
-                  <div
-                    className="icon-items wow fadeInUp"
-                    data-wow-delay=".7s"
-                    style={{
-                      visibility: "hidden",
-                      animationDelay: "0.7s",
-                      animationName: "none",
-                    }}
-                  >
-                    <div className="icon">
-                      <img src={images.icon2} alt="icon-img" />
+                  {features.slice(0, 2).map((feature, index) => (
+                    <div
+                      key={feature.id}
+                      className="icon-items wow fadeInUp"
+                      data-wow-delay={`${0.7 + index * 0.2}s`}
+                    >
+                      <div className="icon">
+                        <img
+                          src={images[`icon${index + 2}`]}
+                          alt="feature-icon"
+                        />
+                      </div>
+                      <div className="content">
+                        <h4>{feature.title}</h4>
+                        {/* <p>{feature.description}</p> */}
+                      </div>
                     </div>
-                    <div className="content">
-                      <h4>Solar Power</h4>
-                      <p>
-                        Lorem ipsum dolor sit amet cut co sect. Proin viverra
-                      </p>
-                    </div>
-                  </div>
-                  <div
-                    className="icon-items wow fadeInUp"
-                    data-wow-delay=".9s"
-                    style={{
-                      visibility: "hidden",
-                      animationDelay: "0.9s",
-                      animationName: "none",
-                    }}
-                  >
-                    <div className="icon">
-                      <img src={images.icon3} alt="icon-img" />
-                    </div>
-                    <div className="content">
-                      <h4>BrightSun Support</h4>
-                      <p>
-                        Lorem ipsum dolor sit amet cut co sect. Proin viverra
-                      </p>
-                    </div>
-                  </div>
+                  ))}
                 </div>
+
+                {/* CTA + Author */}
                 <div className="about-author">
                   <div
                     className="about-button wow fadeInUp"
-                    data-wow-delay=".5s"
-                    style={{
-                      visibility: "hidden",
-                      animationDelay: "0.5s",
-                      animationName: "none",
-                    }}
+                    data-wow-delay="1s"
                   >
-                    <a
-                      href="https://reenergy.baseecom.com/about.html"
-                      className="theme-btn"
-                    >
-                      Explore More
+                    <a href={cta.url} className="theme-btn">
+                      {cta.text}
                       <i className="fa-solid fa-arrow-right-long" />
                     </a>
                   </div>
-                  <div
-                    className="author-image wow fadeInUp"
-                    data-wow-delay=".7s"
-                    style={{
-                      visibility: "hidden",
-                      animationDelay: "0.7s",
-                      animationName: "none",
-                    }}
-                  >
-                    <img src={images.author} alt="author-img" />
-                    <div className="content">
-                      <h6>Ronald Richards</h6>
-                      <p>Co, Founder</p>
+
+                  {author && (
+                    <div
+                      className="author-image wow fadeInUp"
+                      data-wow-delay="1.2s"
+                    >
+                      <img src={images.author} alt="author" />
+                      <div className="content">
+                        <h6>{author.name}</h6>
+                        <p>{author.designation}</p>
+                      </div>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
             </div>
--- app/home/(Components)/Hero.jsx
+++ app/home/(Components)/Hero.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
+import heroData from "../(Data)/Hero.json";
 
 const Hero = () => {
   const swiperRef = useRef(null);
@@ -37,7 +38,8 @@ const Hero = () => {
         loop={true}
         className="hero-slider"
       >
-        <SwiperSlide>
+        {heroData.slides.map((slide, index) => (
+          <SwiperSlide key={index + "a"}>
             <div
               className="hero-image bg-cover"
               style={{
@@ -54,23 +56,21 @@ const Hero = () => {
                       data-duration="2s"
                       data-delay=".3s"
                     >
-                      WELCOME TO THE SOLAR SOLEN{" "}
+                      {slide.tag}
                     </h6>
                     <h1
                       data-animation="slideInRight"
                       data-duration="2s"
                       data-delay=".5s"
                     >
-                      Powering the Future With Renewable.
+                      {slide.title}
                     </h1>
                     <p
                       data-animation="slideInRight"
                       data-duration="2s"
                       data-delay=".7s"
                     >
-                      Transmds is the world’s driving worldwide coordinations
-                      supplier we uphold <br /> industry and exchange the
-                      worldwide trade of merchandi
+                      {slide.desc}
                     </p>
                     <div className="hero-button">
                       <a
@@ -81,7 +81,7 @@ const Hero = () => {
                         data-delay=".9s"
                       >
                         <span>
-                          Learn More <i className="fas fa-chevron-right" />
+                          {slide.btn} <i className="fas fa-chevron-right" />
                         </span>
                       </a>
                     </div>
@@ -89,61 +89,8 @@ const Hero = () => {
                 </div>
               </div>
             </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div
-              className="hero-image bg-cover"
-              style={{
-                backgroundImage:
-                  'url("https://reenergy.baseecom.com/assets/img/hero/hero-4.jpg")',
-              }}
-            />
-            <div className="container">
-              <div className="row g-4">
-                <div className="col-lg-8">
-                  <div className="hero-content">
-                    <h6
-                      data-animation="slideInRight"
-                      data-duration="2s"
-                      data-delay=".3s"
-                    >
-                      {" "}
-                      WELCOME TO THE SOLAR SOLEN{" "}
-                    </h6>
-                    <h1
-                      data-animation="slideInRight"
-                      data-duration="2s"
-                      data-delay=".5s"
-                    >
-                      Powering the Future With Renewable.
-                    </h1>
-                    <p
-                      data-animation="slideInRight"
-                      data-duration="2s"
-                      data-delay=".7s"
-                    >
-                      Transmds is the world’s driving worldwide coordinations
-                      supplier we uphold <br /> industry and exchange the
-                      worldwide trade of merchandi
-                    </p>
-                    <div className="hero-button">
-                      <a
-                        href="index.html"
-                        className="theme-btn theme-color-2"
-                        data-animation="slideInRight"
-                        data-duration="2s"
-                        data-delay=".9s"
-                      >
-                        <span>
-                          Learn More <i className="fas fa-chevron-right" />
-                        </span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
